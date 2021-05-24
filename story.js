9a88d9107ebac2cd657b3ef86560e94653f0c24e
ali mohammed <darkblitz79@gmail.com>
2021-05-24 16:52:46 +0300

    Hello World

--- story.js
+++ story.js
@@ -1,5 +1,5 @@
 let Name = "Ali";
-let Age = "23";
+let Age = 23;
 let Hobby = "gaming";
 
 console.log(`Mr. ${Name}, in 33 years, you will be ${Age + 33} years old. By then you'll have decades of ${Hobby} practice!`);
